8210d186386392a605f728d76edfaf718878afcc
Eric Lindvall <eric@5stops.com>
2014-01-16 00:49:04 -0800

    Add more tests

--- test/slack_hooks/service/travis_test.clj
+++ test/slack_hooks/service/travis_test.clj
@@ -9,4 +9,12 @@
           request         {:params {:payload text}}
           formatted-text  (travis/travis-format request)]
       (is (= "[build] #1 (<https://github.com/eric/north-american-bear/compare/48178e556ef8...1744327fb856|1744327f>) by eric of <https://github.com/eric/north-american-bear|north-american-bear/master> failed in 15s — <https://travis-ci.org/eric/north-american-bear/builds/16906218>"
+             formatted-text))))
+
+
+  (testing "Formatting a Travis CI pull request webhook"
+    (let [text            (slurp "test/resources/travis-ci-pull.json")
+          request         {:params {:payload text}}
+          formatted-text  (travis/travis-format request)]
+      (is (= "[build] #10 (<https://github.com/eric/north-american-bear/pull/1|517eba97>) by eric of <https://github.com/eric/north-american-bear|north-american-bear/pull/1> passed in 17s — <https://travis-ci.org/eric/north-american-bear/builds/17048460>"
              formatted-text)))))
