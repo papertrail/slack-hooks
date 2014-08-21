f3e34ea62bd19aad4da82eb948ea5026e54e3be8
Larry Marburger <larry@marburger.cc>
2014-08-21 10:33:33 -0400

    Pass subset of incident data to (incident-prefix)

--- src/slack_hooks/service/pagerduty.clj
+++ src/slack_hooks/service/pagerduty.clj
@@ -18,8 +18,8 @@
     nil))
 
 (defn incident-description
-  [payload]
-  (let [summary-data (-> payload :data :incident :trigger_summary_data)]
+  [incident]
+  (let [summary-data (:trigger_summary_data incident)]
     (or
       (:subject summary-data)
       (:description summary-data))))
@@ -34,38 +34,38 @@
        (string/join " & ")))
 
 (defn incident-prefix
-  [payload]
-  (condp = (:type payload)
-    "incident.trigger"       (let [service-name (-> payload :data :incident :service :name)
-                                   service-url  (-> payload :data :incident :service :html_url)]
+  [incident incident-type]
+  (condp = incident-type
+    "incident.trigger"       (let [service-name (-> incident :service :name)
+                                   service-url  (-> incident :service :html_url)]
                                (format "New incident from <%s|%s>" service-url service-name))
 
-    "incident.acknowledge"   (let [acknowledgers (-> payload :data :incident :acknowledgers)
+    "incident.acknowledge"   (let [acknowledgers (-> incident :acknowledgers)
                                    names         (format-user-objects acknowledgers)]
                                (format "Acknowledged by %s" names))
 
-    "incident.delegate"      (let [assigned-to (-> payload :data :incident :assigned_to)
+    "incident.delegate"      (let [assigned-to (-> incident :assigned_to)
                                    names       (format-user-objects assigned-to)]
                                (format "Assigned to %s" names))
 
-    "incident.resolve"       (let [resolved-by (-> payload :data :incident :resolved_by_user)
+    "incident.resolve"       (let [resolved-by (-> incident :resolved_by_user)
                                    html-url     (:html_url resolved-by)
                                    name         (:name resolved-by)]
                                (format "Resolved by <%s|%s>" html-url name))
 
-    "incident.assign"        (let [assigned-to (-> payload :data :incident :assigned_to)
+    "incident.assign"        (let [assigned-to (-> incident :assigned_to)
                                    names       (format-user-objects assigned-to)]
                                (format "Assigned to %s" names))
 
-    "incident.escalate"      (let [assigned-to (-> payload :data :incident :assigned_to)
+    "incident.escalate"      (let [assigned-to (-> incident :assigned_to)
                                    names       (format-user-objects assigned-to)]
                                (format "Escalated to %s" names))
 
-    "incident.unacknowledge" (let [assigned-to (-> payload :data :incident :assigned_to)
+    "incident.unacknowledge" (let [assigned-to (-> incident :assigned_to)
                                    names       (format-user-objects assigned-to)]
                                (format "Unacknowledged due to timeout and reassigned to %s" names))
 
-    (format "%s received" (:type payload))))
+    (format "%s received" incident-type)))
 
 (defn formatted-message
   "Returns a description of the given Pagerduty alert."
@@ -73,8 +73,9 @@
   (let [incident        (-> payload :data :incident)
         incident-number (:incident_number incident)
         incident-url    (:html_url incident)
-        prefix          (incident-prefix payload)
-        description     (incident-description payload)]
+        incident-type   (:type payload)
+        prefix          (incident-prefix incident incident-type)
+        description     (incident-description incident)]
     (format "<%s|#%s>: %s: %s"
             incident-url
             incident-number
