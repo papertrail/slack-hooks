98aba8eea535f66faba87798aad774474cd64d39
Jose Pablo Mora <cont@ctjo.se>
2016-02-29 19:48:57 -0600

    Fix text.

--- test/slack_hooks/service/scout_test.clj
+++ test/slack_hooks/service/scout_test.clj
@@ -7,9 +7,9 @@
     (let [text (slurp "test/resources/scout-alert.json")
           request {:params {:payload text}}
           data (scout/scout-message request)]
-      (is (= "[Alert start] Load Average on Blade" (:title data)))
+      (is (= "Load Average on Blade" (:title data)))
       (is (= "https://scoutapp.com/a/999999" (:title_link data)))
-      (is (= "Load Average - Last minute met or exceeded 3.00 , increasing to 3.50 at 01:06AM" (:text data)))
+      (is (= "Last minute met or exceeded 3.00 , increasing to 3.50 at 01:06AM" (:text data)))
       (is (= "[Alert start] Load Average on Blade - https://scoutapp.com/a/999999" (:fallback data)))
       (is (= "danger" (:color data)))
              )))
